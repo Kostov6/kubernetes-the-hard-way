9c4781358a402b2f9bfae33587823a9b6dfa3f28
Viktor <v.kostov@sap.com>
2022-09-05 06:42:11 +0300

    Update controller-last-setup.sh

--- scripts/controller-last-setup.sh
+++ scripts/controller-last-setup.sh
@@ -219,32 +219,3 @@ subjects:
     kind: User
     name: kubernetes
 EOF
-
-# Kubernetes Frontend Load Balancer
-
-{
-
-  gcloud compute http-health-checks create kubernetes \
-    --description "Kubernetes Health Check" \
-    --host "kubernetes.default.svc.cluster.local" \
-    --request-path "/healthz"
-
-  gcloud compute firewall-rules create ${components_prefix}-kubernetes-the-hard-way-allow-health-check \
-    --network ${components_prefix}-kubernetes-the-hard-way \
-    --source-ranges 209.85.152.0/22,209.85.204.0/22,35.191.0.0/16 \
-    --allow tcp
-
-  gcloud compute target-pools create kubernetes-target-pool \
-    --http-health-check kubernetes
-
-  gcloud compute target-pools add-instances kubernetes-target-pool \
-   --instances ${components_prefix}-controller-0,${components_prefix}-controller-1,${components_prefix}-controller-2
-
-  gcloud compute forwarding-rules create kubernetes-forwarding-rule \
-    --address ${KUBERNETES_PUBLIC_ADDRESS} \
-    --ports 6443 \
-    --region $(gcloud config get-value compute/region) \
-    --target-pool kubernetes-target-pool
-}
-
-curl --cacert ca.pem https://${KUBERNETES_PUBLIC_ADDRESS}:6443/version
